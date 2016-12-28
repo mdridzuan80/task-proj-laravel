b741613b8f0b69e41a9078be0d301c73edb47fab
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2016-12-28 20:29:13 +0800

    Link app.js and bootstrap

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -4,11 +4,16 @@
   <meta charset="UTF-8">
   <title>My Task</title>
 </head>
+<link rel="stylesheet" href="/css/app.css" charset="utf-8">
+<script src="js/app.js" charset="utf-8"></script>
 <body>
-  <ul>
-    @foreach($tasks as $task)
-      <li>{{ $task-> title }}</li>
-    @endforeach
-  </ul>
+  <div class="container">
+    <ul>
+      <h1>Task List</h1>
+      @foreach($tasks as $task)
+        <li>{{ $task-> title }}</li>
+      @endforeach
+    </ul>
+  </div>
 </body>
 </html>
